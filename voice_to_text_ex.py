af4c7faf0252f49f7ed70458681abf7b148866fd
Shrishti Sharma <48645595+shrishti1997@users.noreply.github.com>
2020-07-15 12:11:43 +0530

    Update voice_to_text_ex.py

--- voice_to_text_ex.py
+++ voice_to_text_ex.py
@@ -1,11 +1,18 @@
 import speech_recognition as sr
-r = sr.Recognizer();
-with sr.Microphone() as source:
-    Print("Speak anything to test")
-    audio = r.listen(source)
-    text = r.recognize_google(audio)
-    try:
-        print("You said : {}".format(text))
-    except:
-        print("Sorry. We could not hear what you said")
 
+r = sr.Recognizer();
+
+bye="bye"
+
+while(True):
+    with sr.Microphone() as source:
+        print("Speak anything to test")
+        audio = r.listen(source)
+        text = r.recognize_google(audio)
+        try:
+            if text == bye:
+                print("aborting..")
+                break
+            print("You said : {}".format(text))
+        except:
+            print("Sorry. We could not hear what you said")
